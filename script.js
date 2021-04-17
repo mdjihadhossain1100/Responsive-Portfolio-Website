bc6530b866ec0686cc9a6107c7d465eb5a2129eb
MD. JIHAD HOSSAIN <78290427+mdjihadhossain1100@users.noreply.github.com>
2021-04-17 19:58:46 +0600

    Update script.js

--- script.js
+++ script.js
@@ -18,13 +18,19 @@ canceBtn.onclick = function() {
 // Sticky navb
 let val;
 let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+console.log(scrollBtn);
+
   window.onscroll = function(){
     if (document.documentElement.scrollTop > 20) {
       nav.classList.add("sticky");
+      scrollBtn.style.display = "block";
     }else{
-      nav.classList.remove("sticky")
+      nav.classList.remove("sticky");
+      scrollBtn.style.display = "none";
     }
   }
+
 
 // Side Navigation Bar Close While We Click On Navigation Links
 let navLinks = document.querySelectorAll(".menu li a");
